d282c4683264cbbb0e4bf461aa28a958acaa6c96
tylerroliverr <tyler.oliverjames@gmail.com>
2024-06-04 18:40:05 +1000

    reeel stuff

--- app/reel/page.js
+++ app/reel/page.js
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from 'react';
 import getReelData from '../components/getReelData';
 import HoverLink from '../components/hoverLink';
 
@@ -6,20 +8,29 @@ export default async function Reel() {
     const reelData = getReelData();
 
 
+    const [showVideo, setShowVideo] = useState(false);
+
+    const handleWatchClick = () => {
+        setShowVideo(true);
+    };
+
     return (
         <>
-            <HoverLink />
-            <div className="videoWrapper">
-                <iframe
-                    src={"https://player.vimeo.com/video/953370934?h=478beb15a9&autoplay=1&loop=1&title=0&byline=0&portrait=0&controls=0&background=1"}
-                    className="projectVideo"
-                    frameBorder="0"
-                    allow="autoplay; fullscreen; picture-in-picture"
-                ></iframe>
-            </div>
-
+        <HoverLink />
+            {showVideo ? (
+                <div className='projectContainer'>
+                <div className='projectImageContainer'>
+                <div className="videoWrapper">
+                <iframe title="vimeo-player" src="https://player.vimeo.com/video/953370934?h=478beb15a9"    allowfullscreen></iframe>
+                </div>
+                </div>
+                </div>
+            ) : (
+<>
             <div className="playReel">
-                <button className="link">Watch Reel</button>
+                <p className="link" onClick={handleWatchClick}>
+                    Watch Reel
+                </p>
             </div>
 
             <div className="playReelMobile">
@@ -30,9 +41,11 @@ export default async function Reel() {
                     <p className="turnScreen">turn your screen</p>
                 </div>
                 <div className='watchDiv'>
-                    <p className='watchReel'>Watch 1107<span className='trademark'>®</span> Reel</p>
+                    <p className='watchReel' onClick={handleWatchClick}>Watch 1107<span className='trademark'>®</span> Reel</p>
                 </div>
             </div>
+            </>
+        )}
             <div className='rightsContainerMobile'>
                 <p>All Rights Reserved ©</p>
             </div>
